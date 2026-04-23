d07e94d0d97a35eb7541869741d465b25c100c23
asprtyy-glitch <asprtyy@gmail.com>
2026-04-23 21:44:44 +0200

    Update user_project_wrapper.v

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -1,16 +1,9 @@
 `default_nettype none
-module user_project_wrapper #(
-    parameter BITS = 32
-)(
+
+module user_project_wrapper (
 `ifdef USE_POWER_PINS
-    inout vdda1,	// User area 1 3.3V supply
-    inout vdda2,	// User area 2 3.3V supply
-    inout vssa1,	// User area 1 supply ground
-    inout vssa2,	// User area 2 supply ground
-    inout vccd1,	// User area 1 1.8V supply
-    inout vccd2,	// User area 2 1.8V supply
-    inout vsscl,	// User area 1 supply ground
-    inout vsscd,	// User area 2 supply ground
+    inout vccd1,
+    inout vssd1,
 `endif
     input wb_clk_i,
     input wb_rst_i,
@@ -25,23 +18,32 @@ module user_project_wrapper #(
     input  [127:0] la_data_in,
     output [127:0] la_data_out,
     input  [127:0] la_oenb,
-    input  [37:0] io_in,
-    output [37:0] io_out,
-    output [37:0] io_oeb,
-    input  [2:0] user_irq
+    input  [`MPRJ_IO_PADS-1:0] io_in,
+    output [`MPRJ_IO_PADS-1:0] io_out,
+    output [`MPRJ_IO_PADS-1:0] io_oeb,
+    output [2:0] user_irq
 );
 
-    // Pulse_X1 16-Core AI Accelerator Instantiation
-    Pulse_X1_Edge_AI_Core pulse_cluster (
+    Pulse_X1_Edge_AI_Core ai_core (
+    `ifdef USE_POWER_PINS
+        .vccd1(vccd1),
+        .vssd1(vssd1),
+    `endif
         .clk(wb_clk_i),
-        .reset(wb_rst_i),
-        .start_compute(la_data_in),
-        .hbm3_bus_in(la_data_in[63:0]),
-        .liquid_temp_sensor(la_data_in[71:64]),
-        .data_out(la_data_out[63:0]),
-        .process_complete(la_data_out),
-        .system_alerts(la_data_out[67:65]),
-        .pump_ctrl(la_data_out)
+        .rst_n(~wb_rst_i),
+        .wb_stb_i(wbs_stb_i),
+        .wb_cyc_i(wbs_cyc_i),
+        .wb_we_i(wbs_we_i),
+        .wb_sel_i(wbs_sel_i),
+        .wb_dat_i(wbs_dat_i),
+        .wb_adr_i(wbs_adr_i),
+        .wb_ack_o(wbs_ack_o),
+        .wb_dat_o(wbs_dat_o)
     );
+
+    assign user_irq = 3'b000;
+    assign io_out = {`MPRJ_IO_PADS{1'b0}};
+    assign io_oeb = {`MPRJ_IO_PADS{1'b1}};
+    assign la_data_out = 128'b0;
 
 endmodule
