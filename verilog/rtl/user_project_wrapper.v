c306ca8e2a2178866556b0516ce0f1d5ec0a15f8
asprtyy-glitch <asprtyy@gmail.com>
2026-04-25 01:22:15 +0300

    Update user_project_wrapper.v

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -2,9 +2,11 @@
 
 module user_project_wrapper (
 `ifdef USE_POWER_PINS
-    inout vccd1,
-    inout vssd1,
+    inout vccd1,	
+    inout vssd1,	
 `endif
+
+    // Wishbone Slave ports (WB-bus signals)
     input wb_clk_i,
     input wb_rst_i,
     input wbs_stb_i,
@@ -15,35 +17,51 @@ module user_project_wrapper (
     input [31:0] wbs_adr_i,
     output wbs_ack_o,
     output [31:0] wbs_dat_o,
+
+    // Logic Analyzer Signals
     input  [127:0] la_data_in,
     output [127:0] la_data_out,
     input  [127:0] la_oenb,
+
+    // IO Pads
     input  [`MPRJ_IO_PADS-1:0] io_in,
     output [`MPRJ_IO_PADS-1:0] io_out,
     output [`MPRJ_IO_PADS-1:0] io_oeb,
+
+    // IRQ
     output [2:0] user_irq
 );
 
-    Pulse_X1_Edge_AI_Core ai_core (
-    `ifdef USE_POWER_PINS
-        .vccd1(vccd1),
-        .vssd1(vssd1),
-    `endif
+    // Internal wires for Titan Core connection
+    wire [7:0] core_done_flags;
+    wire [7:0] core_pump_ctrl;
+
+    // Titan Core Instance (Pulse_X1_Core_v2)
+    Pulse_X1_Core_v2 titan_core (
         .clk(wb_clk_i),
-        .rst_n(~wb_rst_i),
-        .wb_stb_i(wbs_stb_i),
-        .wb_cyc_i(wbs_cyc_i),
-        .wb_we_i(wbs_we_i),
-        .wb_sel_i(wbs_sel_i),
-        .wb_dat_i(wbs_dat_i),
-        .wb_adr_i(wbs_adr_i),
-        .wb_ack_o(wbs_ack_o),
-        .wb_dat_o(wbs_dat_o)
+        .reset(wb_rst_i),
+        .done_flags(core_done_flags),
+        .pump_ctrl(core_pump_ctrl)
     );
 
-    assign user_irq = 3'b000;
-    assign io_out = {`MPRJ_IO_PADS{1'b0}};
-    assign io_oeb = {`MPRJ_IO_PADS{1'b1}};
+    // Physical Mapping and OEB fix
+    // Setting io_oeb to 0 enables the output buffer for all pins
+    assign io_oeb = 38'b0; 
+
+    // Mapping core outputs to GPIO pins 10-17 and 18-25
+    assign io_out[17:10] = core_done_flags;
+    assign io_out[25:18] = core_pump_ctrl;
+    
+    // Default values for unused pins
+    assign io_out[9:0]   = 10'b0;
+    assign io_out[37:26] = 12'b0;
+
+    // Tie-off unused Caravel signals
+    assign wbs_ack_o = 1'b0;
+    assign wbs_dat_o = 32'b0;
+    assign user_irq  = 3'b0;
     assign la_data_out = 128'b0;
 
 endmodule
+
+`default_nettype wire
